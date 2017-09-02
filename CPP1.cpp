c8c5bed4fcdeea3c5cab48e98bedfc1310ffe17d
Suritra Bandyopadhyay <31152879+SurisGithub@users.noreply.github.com>
2017-09-02 11:59:59 +0530

    Update CPP1.cpp

--- CPP1.cpp
+++ CPP1.cpp
@@ -9,6 +9,6 @@ int main()
   cout<<"Program to find maximum of two numbers";
   cout<<"\n \nEnter two different numbers: ";
   cin>>a>>b;
-  cout<<"\nThe greater number is: "<<((a>=b)?a:b);
+  cout<<"\nThe greater number is: "<<((a>b)?a:b);
   return 0;
 }
