475b522b9f3e6d19511a2a4657733cc82da2b7fd
Suritra Bandyopadhyay <31152879+SurisGithub@users.noreply.github.com>
2017-09-04 15:34:30 +0530

    Create CPP31.cpp

---
+++ CPP31.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+/*Program to count number of digits in any number.*/
+int main()
+{
+  int num=0;
+  int dig=0;
+  cout<<"PROGRAM TO CALCULATE NUMBER OF DIGITS IN THE GIVEN NUMBER";
+  cout<<"\n \nEnter the number(WHOLE NUMBER): ";
+  cin>>num;
+  int num1=num;
+  if(num<0)
+  {
+    cout<<"\n \nTHIS IS NOT A WHOLE NUMBER !  !  !";
+    return 0;
+  }
+  while(num1>0)
+  {
+    num1=num1/10;
+    dig++;
+  }
+  if(num!=0)
+  {
+    cout<<"\n \nThe number of digits in "<<num<<" is "<<dig;
+  }
+  else
+  {
+    cout<<"\n \nThe number of digits in "<<num<<" is 1";
+  }
+  return 0;
+ }
+ 
